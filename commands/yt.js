ff491647b00d0a5cb19e1970b913c3446fc8838a
Sceptero <tir4misu@o2.pl>
2016-08-31 15:38:05 +0200

    improvements to yt

--- commands/yt.js
+++ commands/yt.js
@@ -10,16 +10,19 @@ var ytnode = require('youtube-node');
 
 
 
-var playing = false, converter, vol = 0.1;
+var playing = false, converter, vol = cfg['volume'], title;
 
 function playYT(context, param) {
-    var dl = ytdl(param)
+    var dl = ytdl(param, { filter: function(format) { return format.container == "mp4"; }})
         .on('info', function(info, format) {
-            context.connection.user.channel.sendMessage("[YT PLAYER] Now playing: " + info["title"]);
+            title = info["title"];
+            context.connection.user.channel.sendMessage("[YT PLAYER] Now playing: " + title);
         });
+
     converter = ffmpeg(dl).setFfmpegPath(cfg['ffmpegLocation']).format('mp3').pipe(new lame.Decoder())
         .on('format', function(format) {
             playing = true;
+                            
             this.pipe(context.connection.inputStream({
                 channels: format.channels,
                 sampleRate: format.sampleRate,
@@ -33,7 +36,7 @@ function playYT(context, param) {
 }
 
 module.exports = {
-        command: /!yt (play |stop|vol |find )(.*)/,
+        command: /!yt (play |stop|vol |find |info)(.*)/,
         action: function(context, action, param) {
             action = action.trim();
             param = param.trim().replace(/<(?:.|\n)*?>/gm, '');
@@ -44,18 +47,23 @@ module.exports = {
                     return;
                 }
 
+                if (action==="info") {
+                    if (playing)
+                        context.connection.user.channel.sendMessage("[YT PLAYER] Now playing: " + title);
+                    return;
+                }
+
                 // find song by name
                 if (action==="find") {
                     var yt = new ytnode();
                     yt.setKey(cfg['googleAPIKey']);
                     yt.addParam('type', 'video');
                     yt.search(param, 1, function(er, res) {
-                        if (er) {
+                        if (er || !res.items[0]) {
                             console.error(er);
                             return;
                         }
                         else {
-                            console.log(res.items[0]);
                             param = 'https://www.youtube.com/watch?v=' + res.items[0].id.videoId;
                             playYT(context,param);
                         }
--- config.js
+++ config.js
@@ -6,5 +6,7 @@ module.exports = {
     // Google API Data Key
     'googleAPIKey': 'AIzaSyCIUG_fUAKDgXw6mn5MupsimKJwg0wFKG8',
     // URL of server to connect to (mumble://example-server-address.com)
-    'serverURL': 'mumble://pexu.tk'
+    'serverURL': 'mumble://pexu.tk',
+    // default volume
+    'volume': 0.1
 }
