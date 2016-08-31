46b226c72db7bd008a15c19a91ff7f349fc70a9d
Sceptero <tir4misu@o2.pl>
2016-08-31 13:37:06 +0200

    added config, made !yt find to look only for vids

--- commands/yt.js
+++ commands/yt.js
@@ -1,9 +1,14 @@
-// play music from yt
+// Play music from yt
+
+var cfg = require('../config.js');
+
 var ytdl = require('ytdl-core');
 var lame = require('lame');
 var fs = require('fs');
 var ffmpeg = require('fluent-ffmpeg')
 var ytnode = require('youtube-node');
+
+
 
 var playing = false, converter, vol = 0.1;
 
@@ -12,7 +17,7 @@ function playYT(context, param) {
         .on('info', function(info, format) {
             context.connection.user.channel.sendMessage("[YT PLAYER] Now playing: " + info["title"]);
         });
-    converter = ffmpeg(dl).setFfmpegPath('D:/dev/libs/ffmpeg/bin/ffmpeg.exe').format('mp3').pipe(new lame.Decoder())
+    converter = ffmpeg(dl).setFfmpegPath(cfg['ffmpegLocation']).format('mp3').pipe(new lame.Decoder())
         .on('format', function(format) {
             playing = true;
             this.pipe(context.connection.inputStream({
@@ -42,13 +47,15 @@ module.exports = {
                 // find song by name
                 if (action==="find") {
                     var yt = new ytnode();
-                    yt.setKey('AIzaSyCIUG_fUAKDgXw6mn5MupsimKJwg0wFKG8');
+                    yt.setKey(cfg['googleAPIKey']);
+                    yt.addParam('type', 'video');
                     yt.search(param, 1, function(er, res) {
                         if (er) {
                             console.error(er);
                             return;
                         }
                         else {
+                            console.log(res.items[0]);
                             param = 'https://www.youtube.com/watch?v=' + res.items[0].id.videoId;
                             playYT(context,param);
                         }
