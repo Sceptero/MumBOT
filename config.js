ade13845e0587ee84414badc84adad0a76c649ea
Sceptero <tir4misu@o2.pl>
2016-09-22 00:53:14 +0200

    improvements to commands
    better error handling while loading command modules, removed !comment command (only bot itself will be able to change his comment), implemented !roll command, tweaked regexes and merged the "!yt find" and "!yt play" into a single command "!yt play" handling both urls and search terms.

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 module.exports = {
     // Name of the bot
-    'botName': 'MumBOT',
+    'botName': 'MumBOT2',
     // Location on ffmpeg executable
-    'ffmpegLocation': 'D:/dev/libs/ffmpeg/bin/ffmpeg.exe',
+    'ffmpegLocation': './ffmpeg/ffmpeg.exe',
     // Google API Data Key
     'googleAPIKey': 'AIzaSyCIUG_fUAKDgXw6mn5MupsimKJwg0wFKG8',
     // URL of server to connect to (mumble://example-server-address.com)
     'serverURL': 'mumble://pexu.tk',
     // default volume
-    'volume': 0.1
+    'volume': 0.1,
+    // name of database
+    'dbFileName': 'db'
 }
